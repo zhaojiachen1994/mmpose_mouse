4e79bc545bda72835c6af493e54ddd34ad7769b0
zhaojiachen <zhao_jiachen@163.com>
2023-03-13 17:03:30 +0800

    [Feat] add scripts.py

---
+++ draft/scripts.py
@@ -0,0 +1,3 @@
+# python tools/train.py "configs/mouse/hrnet_w48_dannce_2d_p12_256x256.py" --gpu-id 0
+
+# python tools/test.py "configs/mouse/hrnet_w48_dannce_2d_p12_256x256.py" "work_dirs/hrnet_w48_dannce_2d_p12_256x256/best_AP_epoch_100.pth" --gpu-id 0
